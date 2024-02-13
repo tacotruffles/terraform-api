3c23693034f5feb2a039755df0cf30b41c1adb02
John Dawes <jdawes@bluebitmedia.com>
2024-02-12 17:00:54 -0700

    NDA compliance

--- api/src/api/v1/routes.js
+++ api/src/api/v1/routes.js
@@ -5,10 +5,10 @@ const router = express.Router();
 router.use('/info', require('./routes/info'));
 router.use('/users', require('./routes/users'));
 router.use('/admin', require('./routes/admin')); 
-router.use('/organizations', require('./routes/organizations'));
-router.use('/lessons', require('./routes/lessons'));
-router.use('/comments', require('./routes/comments'));
-router.use('/reflections', require('./routes/reflections'));
+// router.use('/organizations', require('./routes/organizations'));
+// router.use('/lessons', require('./routes/lessons'));
+// router.use('/comments', require('./routes/comments'));
+// router.use('/reflections', require('./routes/reflections'));
 // router.use('/files', require('./routes/files'));
 
 // if(process.env.ENV === 'local'){
